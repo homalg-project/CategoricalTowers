e7f9543807ad0761a8d781df6aa9a02551f26ee7
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2017-03-22 22:24:17 +0100

    fix in docu of HomalgGeneratorsForModule

--- gap/HomalgGenerators.gd
+++ gap/HomalgGenerators.gd
@@ -5,7 +5,7 @@
 #! @Description
 #!  Add the filters <C>IsHomalgGeneratorsOfLeftModule</C> resp. <C>IsHomalgGeneratorsOfRightModule</C>
 #!  to the <C>IsLeftOrRightPresentation</C> <A>rel</A> and return <A>rel</A>.
-#! @Arguments rel
-#! @Returns rel with side effect
+#! @Arguments gens
+#! @Returns gens with side effect
 DeclareOperation( "HomalgGeneratorsForModule",
         [ IsLeftOrRightPresentationMorphism ] );
