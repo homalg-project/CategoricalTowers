aa7e0e69e0b3feae7c6d6c62e5bbd3fc7859fcd0
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2018-08-23 22:19:27 +0200

    declared IsCartesianClosedCategory & IsCocartesianCoclosedCategory

--- gap/HeytingAlgebra.gd
+++ gap/HeytingAlgebra.gd
@@ -4,6 +4,22 @@
 # Declarations
 #
 
+#! @Chapter Heyting algebras
+
+#! @Section GAP Properties
+
+#! @Description
+#!  The property of <A>C</A> being a cartesian closed &CAP; category.
+#! @Arguments C
+DeclareProperty( "IsCartesianClosedCategory",
+                 IsCapCategory );
+
+#! @Description
+#!  The property of <A>C</A> being a co-cartesian co-closed &CAP; category.
+#! @Arguments C
+DeclareProperty( "IsCocartesianCoclosedCategory",
+                 IsCapCategory );
+
 # @Section Tools
 
 DeclareGlobalFunction( "ADD_COMMON_METHODS_FOR_HEYTING_ALGEBRAS" );
