9202f88289a588732406c89c1ca5f8e89f93eb3e
Kamal Saleh <ksaleh87@yahoo.com>
2021-07-19 16:03:49 +0200

    d&i methods to convert cells between
    Hom(A_oid,kmat) and QuiverRepresentations(A)
    and use those to contructe isomorphism functors.

--- gap/Functors.gd
+++ gap/Functors.gd
@@ -11,6 +11,11 @@
 #
 ####################################
 
+DeclareAttribute( "ConvertToCellInHomCategory", IsQuiverRepresentation );
+DeclareAttribute( "ConvertToCellInHomCategory", IsQuiverRepresentationHomomorphism );
+DeclareAttribute( "ConvertToCellInCategoryOfQuiverRepresentations", IsCapCategoryObjectInHomCategory );
+DeclareAttribute( "ConvertToCellInCategoryOfQuiverRepresentations", IsCapCategoryMorphismInHomCategory );
+
 #! @Description
 #! The input is a Hom-category <A>H</A><C>:=Hom(B,C)</C> where <C>B</C> is an algebroid defined by some quiver algebra <C>A</C> and
 #! <C>C</C> is a matrix category over some homalg field <C>K</C>.
