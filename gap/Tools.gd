17954f90fbf6d790b2ffd17515a7e2d66c9b791a
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2022-06-09 14:43:36 +0200

    relocated code

---
+++ gap/Tools.gd
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Toposes: Elementary toposes
+#
+# Declarations
+#
+
+####################################
+##
+#! @Chapter Tools
+##
+####################################
+
+####################################
+##
+#! @Section Visualizing posets of subobjects
+##
+####################################
+
+#! @Description
+#! The argument is an object <A>A</A>.
+#! The output is the reflexive transitive reduction of the poset of all subobjects of <A>A</A>.
+#! @Returns a digraph
+#! @Arguments A
+DeclareOperation( "DigraphOfSubobjects",
+                  [ IsCapCategoryObject ] );
