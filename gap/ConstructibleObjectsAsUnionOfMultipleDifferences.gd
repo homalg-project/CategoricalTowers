ab21c16420d72db34162cbe5839cdc8db667d25c
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2022-07-21 20:50:08 +0200

    declared and set UnderlyingMeetSemilatticeOfMultipleDifferences

--- gap/ConstructibleObjectsAsUnionOfMultipleDifferences.gd
+++ gap/ConstructibleObjectsAsUnionOfMultipleDifferences.gd
@@ -36,6 +36,9 @@ DeclareAttribute( "ListOfNormalizedObjectsInMeetSemilatticeOfMultipleDifferences
 DeclareAttribute( "ListOfStandardObjectsInMeetSemilatticeOfMultipleDifferences",
         IsConstructibleObjectAsUnionOfMultipleDifferences );
 
+DeclareAttribute( "UnderlyingMeetSemilatticeOfMultipleDifferences",
+        IsBooleanAlgebraOfConstructibleObjectsAsUnionOfMultipleDifferences );
+
 #! @Section Operations
 
 #! @Description
