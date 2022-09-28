kamalsaleh/FunctorCategories

Injective Envelope and its associated direct sum decomposition
(when the quiver algebra is admissible)

gap/HomologicalMethods.gd
@@ -17,3 +17,9 @@ DeclareAttribute( "MorphismsFromDirectSumDecompositionOfProjectiveCover", IsCapC
 DeclareAttribute( "DirectSumDecompositionOfProjectiveObject", IsCapCategoryObjectInHomCategory );
 
 DeclareAttribute( "ProjectiveCover", IsCapCategoryObjectInHomCategory );
+
+DeclareAttribute( "MorphismsIntoDirectSumDecompositionOfInjectiveEnvelope", IsCapCategoryObjectInHomCategory );
+
+DeclareAttribute( "DirectSumDecompositionOfInjectiveObject", IsCapCategoryObjectInHomCategory );
+
+DeclareAttribute( "InjectiveEnvelope", IsCapCategoryObjectInHomCategory );

homalg-project/CategoricalTowers

gap/HomologicalMethods.gd
@@ -17,3 +17,9 @@ DeclareAttribute( "ProjectiveCover", IsObjectInFunctorCategory );
 DeclareAttribute( "DualOfObjectInFunctorCategory", IsObjectInFunctorCategory );
 
 DeclareAttribute( "DualOfMorphismInFunctorCategory", IsMorphismInFunctorCategory );
+
+DeclareAttribute( "MorphismsIntoDirectSumDecompositionOfInjectiveEnvelope", IsObjectInFunctorCategory );
+
+DeclareAttribute( "DirectSumDecompositionOfInjectiveObject", IsObjectInFunctorCategory );
+
+DeclareAttribute( "InjectiveEnvelope", IsObjectInFunctorCategory );