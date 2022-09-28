kamalsaleh/FunctorCategories

d&i RadicalFunctor

gap/Functors.gd
@@ -35,3 +35,13 @@ DeclareAttribute( "IsomorphismFromCategoryOfQuiverRepresentations", IsCapHomCate
 #! @Arguments B
 #! @Returns a &CAP; functor
 DeclareAttribute( "YonedaEmbedding", IsAlgebroid );
+
+DeclareAttribute( "RadicalFunctorAttr", IsCapHomCategory );
+
+#! @Description
+#! The input is a Hom-category <A>H</A><C>:=Hom(B,C)</C> where <C>B</C> is an algebroid defined by some quiver algebra <C>A</C> and
+#! <C>C</C> is a matrix category over some homalg field <C>K</C>.
+#! The output is the radical endofunctor on <A>H</A>.
+#! @Arguments H
+#! @Returns a &CAP; functor
+DeclareOperation( "RadicalFunctor", [ IsCapHomCategory ] );

homalg-project/CategoricalTowers

gap/Functors.gd
@@ -115,3 +115,13 @@ DeclareAttribute( "SievesOfPathsToTruth", IsMorphismInFunctorCategory );
 #! @Group SievesOfPathsToTruth
 DeclareOperation( "SievesOfPathsToTruth",
         [ IsFunctorCategory, IsMorphismInFunctorCategory ] );
+
+DeclareAttribute( "RadicalFunctorAttr", IsFunctorCategory );
+
+#! @Description
+#!  The input is a Hom-category <A>H</A><C>:=Hom(B,C)</C>, where <C>B</C> is a f.p. algebroid and
+#!  <C>C</C> is a matrix category over some homalg field <C>K</C>.
+#!  The output is the radical endofunctor on <A>H</A>.
+#! @Arguments H
+#! @Returns a &CAP; functor
+DeclareOperation( "RadicalFunctor", [ IsFunctorCategory ] );