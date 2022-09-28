kamalsaleh/FunctorCategories

Projective Cover and its associated direct sum decomposition
(when the quiver algebra is admissible)

gap/HomologicalMethods.gd
@@ -1,3 +1,15 @@
 
 
+
+
+
+
 DeclareAttribute( "RadicalInclusion", IsCapCategoryObjectInHomCategory );
+
+DeclareOperation( "CoverElementByProjectiveObject", [ IsCapCategoryObjectInHomCategory, IsCapCategoryMorphism, IsInt ] );
+
+DeclareAttribute( "MorphismsFromDirectSumDecompositionOfProjectiveCover", IsCapCategoryObjectInHomCategory );
+
+DeclareAttribute( "DirectSumDecompositionOfProjectiveObject", IsCapCategoryObjectInHomCategory );
+
+DeclareAttribute( "ProjectiveCover", IsCapCategoryObjectInHomCategory );

homalg-project/CategoricalTowers

gap/HomologicalMethods.gd
@@ -5,3 +5,11 @@
 #
 
 DeclareAttribute( "RadicalInclusion", IsObjectInFunctorCategory );
+
+DeclareOperation( "CoverElementByProjectiveObject", [ IsObjectInFunctorCategory, IsCapCategoryMorphism, IsInt ] );
+
+DeclareAttribute( "MorphismsFromDirectSumDecompositionOfProjectiveCover", IsObjectInFunctorCategory );
+
+DeclareAttribute( "DirectSumDecompositionOfProjectiveObject", IsObjectInFunctorCategory );
+
+DeclareAttribute( "ProjectiveCover", IsObjectInFunctorCategory );