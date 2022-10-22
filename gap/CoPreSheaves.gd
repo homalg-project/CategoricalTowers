1dbbc05d7150f4f4069fd578b0fd90f4f74690a2
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2022-10-22 11:27:34 +0200

    CapJitAddTypeSignature( "CoPreSheaves", ... )

--- gap/CoPreSheaves.gd
+++ gap/CoPreSheaves.gd
@@ -214,11 +214,6 @@ DeclareOperation( "ApplyMorphismInCoPreSheafCategoryToObject",
 #
 ####################################
 
-#! @Arguments B
-#! @Group CoPreSheaves
-DeclareAttribute( "CoPreSheaves",
-        IsCapCategory );
-
 #! @Description
 #!  Construct the category <C>CoPreSheaves(</C> <A>B</A>, <A>C</A> <C>)</C>=
 #!  <C>FunctorCategory(</C> <A>B</A>, <A>C</A> <C>)^op</C> of copresheaves
@@ -230,10 +225,19 @@ DeclareAttribute( "CoPreSheaves",
 DeclareOperationWithCache( "CoPreSheaves",
         [ IsCapCategory, IsCapCategory ] );
 
+CapJitAddTypeSignature( "CoPreSheaves", [ IsCapCategory, IsCapCategory ], IsCoPreSheafCategory );
+
 #! @Arguments B, k
 #! @Group CoPreSheaves
 DeclareOperationWithCache( "CoPreSheaves",
         [ IsCapCategory, IsHomalgRing ] );
+
+#! @Arguments B
+#! @Group CoPreSheaves
+DeclareOperationWithCache( "CoPreSheaves",
+        [ IsCapCategory ] );
+
+CapJitAddTypeSignature( "CoPreSheaves", [ IsCapCategory ], IsCoPreSheafCategory );
 
 DeclareOperation( "CreateCoPreSheafByValues", [ IsCoPreSheafCategory, IsList, IsList ] );
 
