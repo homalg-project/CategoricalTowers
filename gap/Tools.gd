eea677ac50f94a82152e25f13c30de447213a5bf
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2022-12-04 15:53:24 +0100

    coding style

--- gap/Tools.gd
+++ gap/Tools.gd
@@ -29,7 +29,12 @@ CapJitAddTypeSignature( "DefiningPairOfUnderlyingQuiver", [ IsFpCategory ],
     return rec( filter := IsNTuple,
                 element_types :=
                 [ rec( filter := IsInt ),
-                  rec( filter := IsList, element_type := rec( filter := IsNTuple, element_types := [ rec( filter := IsInt ), rec( filter := IsInt ) ] ) ) ] );
+                  rec( filter := IsList,
+                       element_type :=
+                       rec( filter := IsNTuple,
+                            element_types :=
+                            [ rec( filter := IsInt ),
+                              rec( filter := IsInt ) ] ) ) ] );
     
 end );
 
