69b8b0fc5f4336622ed43392cc4f76dd3b59fec0
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2022-12-27 17:39:41 +0100

    deleted functional PreSheaves/YonedaEmbeddingFunctionalData
    they are now part of the package CategoricalTowers/PreSheaves

--- gap/PreSheaves.gd
+++ gap/PreSheaves.gd
@@ -136,10 +136,6 @@ CapJitAddTypeSignature( "Range", [ IsObjectInPreSheafCategory ],
 end );
 
 ##
-DeclareAttribute( "PairOfFunctionsOfPreSheaf",
-        IsObjectInPreSheafCategory );
-
-##
 DeclareAttribute( "ValuesOfPreSheaf",
         IsObjectInPreSheafCategory );
 
@@ -154,9 +150,6 @@ CapJitAddTypeSignature( "ValuesOfPreSheaf", [ IsObjectInPreSheafCategory ],
                   rec( filter := IsList, element_type := CapJitDataTypeOfMorphismOfCategory( Range( input_types[1].category ) ) ) ] );
     
 end );
-
-DeclareAttribute( "FunctionOfPreSheafMorphism",
-        IsMorphismInPreSheafCategory );
 
 #! @Description
 #!  Returns the values of the presheaf morphism <A>eta</A> on all objects of the source category of presheaf category <A>PSh</A>.
@@ -180,10 +173,6 @@ end );
 #! @Returns a &CAP; functor or natural transformation
 DeclareAttribute( "UnderlyingCapTwoCategoryCell",
         IsCellInPreSheafCategory );
-
-##
-DeclareAttribute( "YonedaEmbeddingFunctionalData",
-        IsPreSheafCategory );
 
 ##
 DeclareAttribute( "YonedaEmbeddingData",
@@ -304,11 +293,6 @@ DeclareOperationWithCache( "PreSheaves",
         [ IsCapCategory ] );
 
 CapJitAddTypeSignature( "PreSheaves", [ IsCapCategory ], IsPreSheafCategory );
-
-DeclareOperationWithCache( "PreSheavesOfEnrichedCategory",
-        [ IsCapCategory, IsCapCategory ] );
-
-CapJitAddTypeSignature( "PreSheavesOfEnrichedCategory", [ IsCapCategory, IsCapCategory ], IsPreSheafCategory );
 
 DeclareOperationWithCache( "PreSheavesOfFpEnrichedCategory",
         [ IsCapCategory, IsCapCategory ] );
