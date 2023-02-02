e2c6f7ebc36b46317650a596be3a1a61bd47994f
Fabian Zickgraf <f.zickgraf@dashdos.com>
2023-02-02 16:04:01 +0100

    Add output_source/range_getter_string to UniqueMorphism

--- Locales/gap/Proset.autogen.gd
+++ Locales/gap/Proset.autogen.gd
@@ -32,7 +32,7 @@ DeclareOperation( "AddAreIsomorphicForObjectsIfIsHomSetInhabited",
 #! The arguments are a category $C$ and a function $F$.
 #! This operation adds the given function $F$
 #! to the category for the basic operation `UniqueMorphism`.
-#! $F: ( arg2, arg3 ) \mapsto \mathtt{UniqueMorphism}(arg2, arg3)$.
+#! $F: ( A, B ) \mapsto \mathtt{UniqueMorphism}(A, B)$.
 #! @Returns nothing
 #! @Arguments C, F
 DeclareOperation( "AddUniqueMorphism",
