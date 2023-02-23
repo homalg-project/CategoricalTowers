9ebdd17ec24c2bd963456571e4cfb5752276a12a
Fabian Zickgraf <f.zickgraf@dashdos.com>
2023-02-23 17:29:56 +0100

    Fix type signature of DefiningPairOfDecoratedQuiver

--- FunctorCategories/gap/CategoryOfDecoratedQuivers.gd
+++ FunctorCategories/gap/CategoryOfDecoratedQuivers.gd
@@ -67,14 +67,41 @@ CapJitAddTypeSignature( "DefiningPairOfDecoratedQuiver", [ IsObjectInCategoryOfD
     
     Assert( 0, IsCategoryOfDecoratedQuivers( input_types[1].category ) );
     
-    return rec( filter := IsNTuple,
+    return rec(
+        filter := IsNTuple,
+        element_types := [
+            rec(
+                filter := IsNTuple,
                 element_types := [
-                        rec( filter := IsInt ),
-                        rec( filter := IsInt ),
-                        rec( filter := IsList,
-                             element_type := rec(
-                                     filter := IsNTuple,
-                                     element_types := [ rec( filter := IsInt ), rec( filter := IsInt ) ] ) ) ] );
+                    rec( filter := IsInt ),
+                    rec( filter := IsInt ),
+                    rec(
+                        filter := IsList,
+                        element_type := rec(
+                            filter := IsNTuple,
+                            element_types := [
+                                rec( filter := IsInt ),
+                                rec( filter := IsInt ),
+                            ],
+                        ),
+                    ),
+                ],
+            ),
+            rec(
+                filter := IsNTuple,
+                element_types := [
+                    rec(
+                        filter := IsList,
+                        element_type := rec( filter := IsInt ),
+                    ),
+                    rec(
+                        filter := IsList,
+                        element_type := rec( filter := IsInt ),
+                    ),
+                ],
+            ),
+        ],
+    );
     
 end );
 
