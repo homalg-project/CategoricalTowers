e6e7caa92cafbe54d68188687ff24a39fedcd721
Fabian Zickgraf <f.zickgraf@dashdos.com>
2023-03-20 11:18:28 +0100

    Apply PackageJanitor

--- dev/upload_codecov.sh
+++ dev/upload_codecov.sh
@@ -23,22 +23,79 @@ shasum -a 256 -c codecov.SHA256SUM
 
 # execute
 chmod +x codecov
-./codecov -Z -v -s ../ -F Algebroids || (sleep 60; ./codecov -Z -v -s ../ -F Algebroids || (sleep 60; ./codecov -Z -v -s ../ -F Algebroids))
-./codecov -Z -v -s ../ -F CategoriesWithAmbientObjects || (sleep 60; ./codecov -Z -v -s ../ -F CategoriesWithAmbientObjects || (sleep 60; ./codecov -Z -v -s ../ -F CategoriesWithAmbientObjects))
-./codecov -Z -v -s ../ -F CategoryConstructor || (sleep 60; ./codecov -Z -v -s ../ -F CategoryConstructor || (sleep 60; ./codecov -Z -v -s ../ -F CategoryConstructor))
-./codecov -Z -v -s ../ -F CatReps || (sleep 60; ./codecov -Z -v -s ../ -F CatReps || (sleep 60; ./codecov -Z -v -s ../ -F CatReps))
-./codecov -Z -v -s ../ -F DiscreteCategories || (sleep 60; ./codecov -Z -v -s ../ -F DiscreteCategories || (sleep 60; ./codecov -Z -v -s ../ -F DiscreteCategories))
-./codecov -Z -v -s ../ -F ExteriorPowersCategories || (sleep 60; ./codecov -Z -v -s ../ -F ExteriorPowersCategories || (sleep 60; ./codecov -Z -v -s ../ -F ExteriorPowersCategories))
-./codecov -Z -v -s ../ -F FiniteCocompletion || (sleep 60; ./codecov -Z -v -s ../ -F FiniteCocompletion || (sleep 60; ./codecov -Z -v -s ../ -F FiniteCocompletion))
-./codecov -Z -v -s ../ -F FunctorCategories || (sleep 60; ./codecov -Z -v -s ../ -F FunctorCategories || (sleep 60; ./codecov -Z -v -s ../ -F FunctorCategories))
-./codecov -Z -v -s ../ -F GradedCategories || (sleep 60; ./codecov -Z -v -s ../ -F GradedCategories || (sleep 60; ./codecov -Z -v -s ../ -F GradedCategories))
-./codecov -Z -v -s ../ -F InternalModules || (sleep 60; ./codecov -Z -v -s ../ -F InternalModules || (sleep 60; ./codecov -Z -v -s ../ -F InternalModules))
-./codecov -Z -v -s ../ -F IntrinsicCategories || (sleep 60; ./codecov -Z -v -s ../ -F IntrinsicCategories || (sleep 60; ./codecov -Z -v -s ../ -F IntrinsicCategories))
-./codecov -Z -v -s ../ -F IntrinsicGradedModules || (sleep 60; ./codecov -Z -v -s ../ -F IntrinsicGradedModules || (sleep 60; ./codecov -Z -v -s ../ -F IntrinsicGradedModules))
-./codecov -Z -v -s ../ -F IntrinsicModules || (sleep 60; ./codecov -Z -v -s ../ -F IntrinsicModules || (sleep 60; ./codecov -Z -v -s ../ -F IntrinsicModules))
-./codecov -Z -v -s ../ -F LazyCategories || (sleep 60; ./codecov -Z -v -s ../ -F LazyCategories || (sleep 60; ./codecov -Z -v -s ../ -F LazyCategories))
-./codecov -Z -v -s ../ -F Locales || (sleep 60; ./codecov -Z -v -s ../ -F Locales || (sleep 60; ./codecov -Z -v -s ../ -F Locales))
-./codecov -Z -v -s ../ -F PreSheaves || (sleep 60; ./codecov -Z -v -s ../ -F PreSheaves || (sleep 60; ./codecov -Z -v -s ../ -F PreSheaves))
-./codecov -Z -v -s ../ -F SubcategoriesForCAP || (sleep 60; ./codecov -Z -v -s ../ -F SubcategoriesForCAP || (sleep 60; ./codecov -Z -v -s ../ -F SubcategoriesForCAP))
-./codecov -Z -v -s ../ -F Toposes || (sleep 60; ./codecov -Z -v -s ../ -F Toposes || (sleep 60; ./codecov -Z -v -s ../ -F Toposes))
-./codecov -Z -v -s ../ -F ZariskiFrames || (sleep 60; ./codecov -Z -v -s ../ -F ZariskiFrames || (sleep 60; ./codecov -Z -v -s ../ -F ZariskiFrames))
+while ! ./codecov -Z -v -s ../ -F Algebroids; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F CategoriesWithAmbientObjects; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F CategoryConstructor; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F CatReps; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F DiscreteCategories; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F ExteriorPowersCategories; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F FiniteCocompletion; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F FunctorCategories; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F GradedCategories; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F InternalModules; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F IntrinsicCategories; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F IntrinsicGradedModules; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F IntrinsicModules; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F LazyCategories; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F Locales; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F PreSheaves; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F SubcategoriesForCAP; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F Toposes; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
+while ! ./codecov -Z -v -s ../ -F ZariskiFrames; do
+    echo "Codecov upload failed, retrying in 60s"
+    sleep 60
+done
