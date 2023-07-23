homalg-project/CategoricalTowers

commit 81346b2ddceb9f54ef589e76dcf22c8a32b2e337

renamed (Co)YonedaEmbeddingOfUnderlyingCategory -> EmbeddingOfUnderlyingCategory
reasons:
* when composing such free constructions there is no natural way to choose between Yoneda and CoYoneda
* these embeddings are the obvious embeddings while the (Co)Yoneda embedding is a lemma

-> stop the inflationary use of the prefix Yoneda and restrict it to the original context

FiniteCocompletions/PackageInfo.g
@@ -10,7 +10,7 @@ SetPackageInfo( rec(
 
 PackageName := "FiniteCocompletions",
 Subtitle := "Finite (co)product/(co)limit (co)completions",
-Version := "2023.07-07",
+Version := "2023.07-08",
 Date := ~.Version{[ 1 .. 10 ]},
 Date := (function ( ) if IsBound( GAPInfo.SystemEnvironment.GAP_PKG_RELEASE_DATE ) then return GAPInfo.SystemEnvironment.GAP_PKG_RELEASE_DATE; else return Concatenation( ~.Version{[ 1 .. 4 ]}, "-", ~.Version{[ 6, 7 ]}, "-01" ); fi; end)( ),
 License := "GPL-2.0-or-later",

FiniteCocompletions/gap/FiniteStrictCoproductCocompletion.gi
@@ -1115,36 +1115,36 @@ InstallMethod( FiniteStrictCoproductCocompletion,
 end );
 
 ##
-InstallMethodForCompilerForCAP( YonedaEmbeddingOfUnderlyingCategoryData,
+InstallMethodForCompilerForCAP( EmbeddingOfUnderlyingCategoryData,
         "for a finite coproduct cocompletion category",
         [ IsFiniteStrictCoproductCocompletion ],
         
   function( UC )
-    local yoneda_embedding_on_objects, yoneda_embedding_on_morphisms;
+    local embedding_on_objects, embedding_on_morphisms;
     
-    yoneda_embedding_on_objects :=
+    embedding_on_objects :=
       objC -> ObjectConstructor( UC, Pair( 1, [ objC ] ) );
     
-    yoneda_embedding_on_morphisms :=
+    embedding_on_morphisms :=
       { source, morC, range } -> MorphismConstructor( UC, source, Pair( [ 0 ], [ morC ] ), range );
     
     return Triple( UnderlyingCategory( UC ),
-                   Pair( yoneda_embedding_on_objects, yoneda_embedding_on_morphisms ),
+                   Pair( embedding_on_objects, embedding_on_morphisms ),
                    UC );
     
 end );
 
 ##
-InstallMethod( YonedaEmbeddingOfUnderlyingCategory,
+InstallMethod( EmbeddingOfUnderlyingCategory,
         "for a finite coproduct cocompletion category",
         [ IsFiniteStrictCoproductCocompletion ],
         
   function( UC )
     local data, Y;
     
-    data := YonedaEmbeddingOfUnderlyingCategoryData( UC );
+    data := EmbeddingOfUnderlyingCategoryData( UC );
     
-    Y := CapFunctor( "Yoneda embedding functor", data[1], UC );
+    Y := CapFunctor( "Embedding functor into a finite strict coproduct cocompletion category", data[1], UC );
     
     AddObjectFunction( Y, data[2][1] );
     
@@ -1166,7 +1166,7 @@ InstallMethod( \.,
     
     C := UnderlyingCategory( UC );
     
-    Y := YonedaEmbeddingOfUnderlyingCategory( UC );
+    Y := EmbeddingOfUnderlyingCategory( UC );
     
     Yc := Y( C.(name) );
     
@@ -1312,13 +1312,13 @@ InstallMethod( ExtendFunctorToFiniteStrictCoproductCocompletion,
 end );
 
 ##
-InstallMethod( ExtendYonedaEmbeddingToFiniteStrictCoproductCocompletion,
+InstallMethod( ExtendEmbeddingToFiniteStrictCoproductCocompletion,
         "for a CAP category",
         [ IsCapCategory ],
         
   function( C )
     
-    return ExtendFunctorToFiniteStrictCoproductCocompletion( YonedaEmbedding( C ) );
+    return ExtendFunctorToFiniteStrictCoproductCocompletion( Embedding( C ) );
     
 end );
 

FunctorCategories/PackageInfo.g
@@ -10,7 +10,7 @@ SetPackageInfo( rec(
 
 PackageName := "FunctorCategories",
 Subtitle := "Categories of functors",
-Version := "2023.07-11",
+Version := "2023.07-12",
 
 Date := ~.Version{[ 1 .. 10 ]},
 Date := (function ( ) if IsBound( GAPInfo.SystemEnvironment.GAP_PKG_RELEASE_DATE ) then return GAPInfo.SystemEnvironment.GAP_PKG_RELEASE_DATE; else return Concatenation( ~.Version{[ 1 .. 4 ]}, "-", ~.Version{[ 6, 7 ]}, "-01" ); fi; end)( ),

FiniteCocompletions/examples/CartesianBraiding.g
@@ -78,11 +78,13 @@ Display( Emb );
 #!   V
 #! LazyCategory(
 #! FiniteStrictProductCompletion( FreeCategory( RightQuiver( "Q(a,b)[]" ) ) ) )
-F := PreCompose( CoYonedaEmbeddingOfUnderlyingCategory( PC ), Emb );
-#! Precomposition of CoYoneda embedding functor and
+F := PreCompose( EmbeddingOfUnderlyingCategory( PC ), Emb );
+#! Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category
 Display( F );
-#! Precomposition of CoYoneda embedding functor and
+#! Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category:
 #! 
 #! FreeCategory( RightQuiver( "Q(a,b)[]" ) )
@@ -91,12 +93,12 @@ Display( F );
 #! LazyCategory(
 #! FiniteStrictProductCompletion( FreeCategory( RightQuiver( "Q(a,b)[]" ) ) ) )
 G := ExtendFunctorToFiniteStrictProductCompletion( F );
-#! Extension to FiniteStrictProductCompletion( Source(
-#! Precomposition of CoYoneda embedding functor and
+#! Extension to FiniteStrictProductCompletion( Source( Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category ) )
 Display( G );
-#! Extension to FiniteStrictProductCompletion( Source(
-#! Precomposition of CoYoneda embedding functor and
+#! Extension to FiniteStrictProductCompletion( Source( Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category ) ):
 #! 
 #! FiniteStrictProductCompletion( FreeCategory( RightQuiver( "Q(a,b)[]" ) ) )

FiniteCocompletions/examples/CartesianDiagonal.g
@@ -85,11 +85,13 @@ Display( Emb );
 #!   V
 #! LazyCategory(
 #! FiniteStrictProductCompletion( FreeCategory( RightQuiver( "Q(a)[]" ) ) ) )
-F := PreCompose( CoYonedaEmbeddingOfUnderlyingCategory( PC ), Emb );
-#! Precomposition of CoYoneda embedding functor and
+F := PreCompose( EmbeddingOfUnderlyingCategory( PC ), Emb );
+#! Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category
 Display( F );
-#! Precomposition of CoYoneda embedding functor and
+#! Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category:
 #! 
 #! FreeCategory( RightQuiver( "Q(a)[]" ) )
@@ -98,12 +100,12 @@ Display( F );
 #! LazyCategory(
 #! FiniteStrictProductCompletion( FreeCategory( RightQuiver( "Q(a)[]" ) ) ) )
 G := ExtendFunctorToFiniteStrictProductCompletion( F );
-#! Extension to FiniteStrictProductCompletion( Source(
-#! Precomposition of CoYoneda embedding functor and
+#! Extension to FiniteStrictProductCompletion( Source( Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category ) )
 Display( G );
-#! Extension to FiniteStrictProductCompletion( Source(
-#! Precomposition of CoYoneda embedding functor and
+#! Extension to FiniteStrictProductCompletion( Source( Precomposition of
+#! Embedding functor into a finite strict product completion category and
 #! Embedding functor into lazy category ) ):
 #! 
 #! FiniteStrictProductCompletion( FreeCategory( RightQuiver( "Q(a)[]" ) ) )

FiniteCocompletions/examples/CocartesianAssociator.g
@@ -59,11 +59,13 @@ Display( Emb );
 #!   V
 #! LazyCategory(
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a,b,c)[]" ) ) ) )
-F := PreCompose( YonedaEmbeddingOfUnderlyingCategory( UC ), Emb );
-#! Precomposition of Yoneda embedding functor and
+F := PreCompose( EmbeddingOfUnderlyingCategory( UC ), Emb );
+#! Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category
 Display( F );
-#! Precomposition of Yoneda embedding functor and
+#! Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category:
 #! 
 #! FreeCategory( RightQuiver( "Q(a,b,c)[]" ) )
@@ -72,12 +74,12 @@ Display( F );
 #! LazyCategory(
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a,b,c)[]" ) ) ) )
 G := ExtendFunctorToFiniteStrictCoproductCocompletion( F );
-#! Extension to FiniteStrictCoproductCocompletion( Source(
-#! Precomposition of Yoneda embedding functor and
+#! Extension to FiniteStrictCoproductCocompletion( Source( Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category ) )
 Display( G );
-#! Extension to FiniteStrictCoproductCocompletion( Source(
-#! Precomposition of Yoneda embedding functor and
+#! Extension to FiniteStrictCoproductCocompletion( Source( Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category ) ):
 #! 
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a,b,c)[]" ) ) )

FiniteCocompletions/examples/CocartesianBraiding.g
@@ -73,11 +73,13 @@ Display( Emb );
 #!   V
 #! LazyCategory(
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a,b)[]" ) ) ) )
-F := PreCompose( YonedaEmbeddingOfUnderlyingCategory( UC ), Emb );
-#! Precomposition of Yoneda embedding functor and
+F := PreCompose( EmbeddingOfUnderlyingCategory( UC ), Emb );
+#! Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category
 Display( F );
-#! Precomposition of Yoneda embedding functor and
+#! Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category:
 #! 
 #! FreeCategory( RightQuiver( "Q(a,b)[]" ) )
@@ -86,12 +88,12 @@ Display( F );
 #! LazyCategory(
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a,b)[]" ) ) ) )
 G := ExtendFunctorToFiniteStrictCoproductCocompletion( F );
-#! Extension to FiniteStrictCoproductCocompletion( Source(
-#! Precomposition of Yoneda embedding functor and
+#! Extension to FiniteStrictCoproductCocompletion( Source( Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category ) )
 Display( G );
-#! Extension to FiniteStrictCoproductCocompletion( Source(
-#! Precomposition of Yoneda embedding functor and
+#! Extension to FiniteStrictCoproductCocompletion( Source( Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category ) ):
 #! 
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a,b)[]" ) ) )

FiniteCocompletions/examples/CocartesianCodiagonal.g
@@ -82,11 +82,13 @@ Display( Emb );
 #!   V
 #! LazyCategory(
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a)[]" ) ) ) )
-F := PreCompose( YonedaEmbeddingOfUnderlyingCategory( UC ), Emb );
-#! Precomposition of Yoneda embedding functor and
+F := PreCompose( EmbeddingOfUnderlyingCategory( UC ), Emb );
+#! Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category
 Display( F );
-#! Precomposition of Yoneda embedding functor and
+#! Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category:
 #! 
 #! FreeCategory( RightQuiver( "Q(a)[]" ) )
@@ -95,12 +97,12 @@ Display( F );
 #! LazyCategory(
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a)[]" ) ) ) )
 G := ExtendFunctorToFiniteStrictCoproductCocompletion( F );
-#! Extension to FiniteStrictCoproductCocompletion( Source(
-#! Precomposition of Yoneda embedding functor and
+#! Extension to FiniteStrictCoproductCocompletion( Source( Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category ) )
 Display( G );
-#! Extension to FiniteStrictCoproductCocompletion( Source(
-#! Precomposition of Yoneda embedding functor and
+#! Extension to FiniteStrictCoproductCocompletion( Source( Precomposition of
+#! Embedding functor into a finite strict coproduct cocompletion category and
 #! Embedding functor into lazy category ) ):
 #! 
 #! FiniteStrictCoproductCocompletion( FreeCategory( RightQuiver( "Q(a)[]" ) ) )

commit b85e734331ee209c5e5357ec36d0f24fe44f81d4

fixed an error introduced in commit 81346b2d

FiniteCocompletions/PackageInfo.g
@@ -10,7 +10,7 @@ SetPackageInfo( rec(
 
 PackageName := "FiniteCocompletions",
 Subtitle := "Finite (co)product/(co)limit (co)completions",
-Version := "2023.07-09",
+Version := "2023.07-10",
 Date := ~.Version{[ 1 .. 10 ]},
 Date := (function ( ) if IsBound( GAPInfo.SystemEnvironment.GAP_PKG_RELEASE_DATE ) then return GAPInfo.SystemEnvironment.GAP_PKG_RELEASE_DATE; else return Concatenation( ~.Version{[ 1 .. 4 ]}, "-", ~.Version{[ 6, 7 ]}, "-01" ); fi; end)( ),
 License := "GPL-2.0-or-later",

FiniteCocompletions/gap/FiniteStrictCoproductCocompletion.gi
@@ -1318,7 +1318,7 @@ InstallMethod( ExtendEmbeddingToFiniteStrictCoproductCocompletion,
         
   function( C )
     
-    return ExtendFunctorToFiniteStrictCoproductCocompletion( Embedding( C ) );
+    return ExtendFunctorToFiniteStrictCoproductCocompletion( YonedaEmbedding( C ) );
     
 end );
 