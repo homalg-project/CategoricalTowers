homalg-project/CategoricalTowers

commit b22bcebf0328e88487e8d0f1a730a8daf0853b69

d&i SET_RANGE_CATEGORY_Of_HOMOMORPHISM_STRUCTURE and used it to simplify code

FunctorCategories/gap/HomStructure.gi
@@ -262,11 +262,9 @@ InstallGlobalFunction( ADD_FUNCTIONS_FOR_HOMOMORPHISM_STRUCTURE_TO_PRESHEAF_CATE
         return;
     fi;
     
-    ##
-    SetRangeCategoryOfHomomorphismStructure( PSh, H );
-
-    ##
-    SetIsEquippedWithHomomorphismStructure( PSh, true );
+    if not HasRangeCategoryOfHomomorphismStructure( H ) then
+        SET_RANGE_CATEGORY_Of_HOMOMORPHISM_STRUCTURE( PSh, H );
+    fi;
     
     ## Be sure the above assignment succeeded:
     Assert( 0, IsIdenticalObj( H, RangeCategoryOfHomomorphismStructure( PSh ) ) );

commit 22594ba592b26caec3b452426571ab936995e6d6

fixed bug in ADD_FUNCTIONS_FOR_HOMOMORPHISM_STRUCTURE_TO_PRESHEAF_CATEGORY
introduced in commit b22bcebf0328e88487e8d0f1a730a8daf0853b69

FunctorCategories/gap/HomStructure.gi
@@ -262,7 +262,7 @@ InstallGlobalFunction( ADD_FUNCTIONS_FOR_HOMOMORPHISM_STRUCTURE_TO_PRESHEAF_CATE
         return;
     fi;
     
-    if not HasRangeCategoryOfHomomorphismStructure( H ) then
+    if not HasRangeCategoryOfHomomorphismStructure( PSh ) then
         SET_RANGE_CATEGORY_Of_HOMOMORPHISM_STRUCTURE( PSh, H );
     fi;
     